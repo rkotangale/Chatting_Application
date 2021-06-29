dd20e1451dafa1c6a2b728356c45ba7b8e8aaacd
Rajat Kotangale <68765846+rkotangale@users.noreply.github.com>
2021-06-29 14:19:14 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3nujHFtNF69_c_rYvvOcjTjJ7KQRRYqU",
+    authDomain: "whatsapp-mern-a9d31.firebaseapp.com",
+    projectId: "whatsapp-mern-a9d31",
+    storageBucket: "whatsapp-mern-a9d31.appspot.com",
+    messagingSenderId: "572081851882",
+    appId: "1:572081851882:web:326d53995edaceaceb3693"
+  };
